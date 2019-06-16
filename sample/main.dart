40359be0109dea586a17a54494f5f2c69fe59d0c
Rabia Abismail <abismail2010@gmail.com>
2019-06-16 22:05:42 +0300

    added sample code and update pubspec.yaml

---
+++ sample/main.dart
@@ -0,0 +1,66 @@
+import 'package:advanced_page_slider/PageSlider.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Advanced PageView Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Advanced PageView Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: PageSlider(
+        //height: MediaQuery.of(context).size.height / 2,
+        initialPage: 0,
+        hidePaginationIndexer: false,
+        disableSWiping: false,
+        hideSliderIndicator: false,
+        sliderIndicatorPosition: SliderIndicatorPosition.BOTTOM,
+        overlaySliderIndicator: true,
+        onPageChanged: (page) => print("page $page"),
+        reverse: false,
+        scrollDirection: Axis.horizontal,
+        widgets: [
+          Container(
+            child: Center(
+              child: Text("text 1"),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text("text 2"),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text("text 3"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:advanced_page_slider/main.dart';
+import '../sample/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
